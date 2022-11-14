1110400d8059c8418abf0dbb528c1427498d380f
snakcy506 <117868224+snakcy506@users.noreply.github.com>
2022-11-14 17:19:24 -0600

    Add files via upload

---
+++ import itertools.py
@@ -0,0 +1,25 @@
+import itertools
+import sys
+import argparse
+
+
+
+name1 = "ana"
+name2 = "luis"
+
+str_1 = list(name1.lower())
+str_2 = list(name2.lower())
+
+print('Permutaciones de', str_1)
+
+for char1 in itertools.permutations(str_1):
+    if char1 == char1[::-1]:
+        print(char1, "palindromo")
+    else:
+        print(char1)
+
+for char2 in itertools.permutations(str_2):
+    if char2 == char2[::-1]:
+        print(char2, "palindromo")
+    else:
+        print(char2)
